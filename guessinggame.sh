9d553bbcbadda04000f16e2f8cb559b1c14c9ca3
Luis Alfonso Suarez Moncada <luisalfonso2297@gmail.com>
2020-08-08 21:27:28 -0500

    Adding a function

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,29 +1,42 @@
 #!/usr/bin/env bash
 
-echo "Hello! Welcome to this game"
-echo "First, tell me your name please" 
-read name
+function mainProcess {
+	echo "*******************************"
+	echo "Hello! Welcome to GuessingGame"
+	echo "*******************************"
+	echo ""
+	echo "First, tell me your name please" 
+	read name
 
-#Finding the correct answer.
-answer=$(ls | wc -l)
+	#Finding the correct answer.
+	answer=$(ls | wc -l)
 
-# If win is 0, the gamen still not win.
-win=0
-while [[ win -eq 0 ]] 
-do
-	echo "Guess!, how many files are in the current directory?"
-	read userAnswer
-
-	if [[ $userAnswer -eq $answer ]]
-	then
-		echo "!!!Contratulations $name, you WIN!!!"
-		let win=$win+1
-	else
-		if [[ $userAnswer -gt $answer ]]
+	# If win is 0, the gamer still not win.
+	win=0
+	while [[ win -eq 0 ]] 
+	do
+		echo "Guess!, how many files are in the current directory?"
+		read userAnswer
+	
+		if [[ $userAnswer -eq $answer ]]
 		then
-			echo "[!] Fail, the number of files is less"
+			echo "************************************"
+			echo "!!!Congratulations $name, you WIN!!!"
+			echo "************************************"
+			let win=$win+1
 		else
-			echo "[!] Fail, the number of files is more"
+			if [[ $userAnswer -gt $answer ]]
+			then
+				echo "-------------------------------------"
+				echo "[!] Fail, the number of files is less"
+				echo "-------------------------------------"
+			else
+				echo "-------------------------------------"
+				echo "[!] Fail, the number of files is more"
+				echo "-------------------------------------"
+			fi
 		fi
-	fi
-done
+	done
+}
+
+mainProcess
